39a6db010e12bdcf2e3a888dabfacd36378f75c5
GretaFormica <greta.formica@gmail.com>
2023-04-25 21:19:38 +0200

    create api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Resource::resource('project', ProjectController::class);
+
+Route::get('/test', function(){
+    $names = ['ciao', 'mario', 'luigi'];
+
+    return response()->json($names);
 });
